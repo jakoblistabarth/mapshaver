1d653827cba6241444cf635ab5329ae734fe9fcc
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-09-11 00:16:12 +0200

    :bug: Place brand on top of the canvas grid

--- app/components/Brand.tsx
+++ app/components/Brand.tsx
@@ -8,7 +8,7 @@ import useAppStore from "../helpers/store";
 const Brand = () => {
   const { isDebug } = useAppStore((state) => state);
   return (
-    <div className="relative flex items-center">
+    <div className="relative z-10 flex items-center">
       <Image alt="Mapshaver Logo" src={brand} className="mr-2 w-4" />
       <h1 className={clsx("font-display text-lg", isDebug && "text-blue-600")}>
         Mapshaver
